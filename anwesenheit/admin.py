6f8b5a1120dbfb743f192f0dd01a6931f37d5ee2
Hendrik <162701586+Souvis17@users.noreply.github.com>
2025-05-22 17:57:20 +0200

    Formular

--- anwesenheit/admin.py
+++ anwesenheit/admin.py
@@ -8,6 +8,18 @@ class AnwesenheitForm(forms.ModelForm):             # neue Klasse, Django-Formul
     schueler = forms.ModelChoiceField(queryset=Schueler.objects.all(),)required=False)  # Dropdownmenü mit Schüler, nicht zwingend erforderlich
     lehrer = forms.ModelChoiceField(queryset=Lehrer.objects.all(), required=False)      # Dropdownmenü mit Lehrer, nicht zwingend erforderlich
 
-class Meta:
-    model = Anwesenheit
-    fields = ['datum', 'uhrzeit', 'anwesend', 'schueler', 'lehrer']
+    class Meta:                                         # Metaklasse
+        model = Anwesenheit                             # Formular basiert auf der Anwesenheitsklasse
+        fields = ['datum', 'uhrzeit', 'anwesend', 'schueler', 'lehrer']     # Daten die angezeigt werden sollen
+
+    def speichern(self, speichern_direkt=True):     # self, akkutelle Instanz des Objekt, commit=True -> speichert das Objekt sofort
+        instanz = super().save(commit=False)        # super() ist python funktion die Methoden aus Eltrnklasse aufruft, save ruft Methode der Elternklasse (ModelForm) auf, commmit=false speichert es noch nicht 
+        if self.cleaned_data['schueler']:           # self.cleaned_data ist ein dictionary(von django) mit allen formular daten, wurde im feld schueler schon etwas ausgewählt
+            instanz.content_type = ContentType.objects.get_for_model(Schueler)
+            instanz.object_id = self.cleaned_data['schueler'].id
+        elif self.cleaned_data['lehrer']:
+            instanz.content_type = ContentType.objects.get_for_model(Lehrer)
+            instanz.object_id = self.cleaned_data['lehrer'].id
+        if speichern_direkt:
+            instanz.save()
+        return instanz
